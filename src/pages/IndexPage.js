f33e41b62f1d966794923767a9023389cdb2421d
Jinyao <you@example.com>
2019-09-25 00:42:22 -0400

    first commit

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './IndexPage.scss';
+import { Layout} from 'antd';
+
+const { Header, Content} = Layout;
+
+function IndexPage() {
+  return (
+    <Layout className={styles.layout}>
+    <Header className={styles.header}>header</Header>
+
+      <Content className={styles.content}>main content</Content>
+  </Layout>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
