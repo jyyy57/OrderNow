eb3ac02e7ba230574181a191894619ed14afbd4f
Jinyao <you@example.com>
2019-09-25 11:46:54 -0400

    update NavBar

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.scss';
+import NavBar from './NavBar';
 import { Layout} from 'antd';
 
 const { Header, Content} = Layout;
@@ -8,7 +9,9 @@ const { Header, Content} = Layout;
 function IndexPage() {
   return (
     <Layout className={styles.layout}>
-    <Header className={styles.header}>header</Header>
+      <Header className={styles.header}>
+        <NavBar />
+    </Header>
 
       <Content className={styles.content}>main content</Content>
   </Layout>
