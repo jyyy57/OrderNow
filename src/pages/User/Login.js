9db4cc0a25752fd559cd0235de8784487fc92e2d
Jinyao <you@example.com>
2019-09-29 20:08:07 -0400

    add register and signin

--- src/pages/User/Login.js
+++ src/pages/User/Login.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react'
+import style from "./account.scss";
+import {Button, Form, Input} from "antd";
+import Logo from 'Assets/icon.png'
 
 export default class index extends Component{
   render() {
     return (
-      <div>
-        Login
-      </div>
+      <div className={style.account}>
+      <img src={Logo} alt='my logo' className={style.logo} />
+      <Form className='account-form'>
+        <Form.Item label='Email'>
+          <Input/>
+        </Form.Item>
+        <Form.Item label='Password'>
+          <Input/>
+        </Form.Item>
+        <Form.Item>
+          <Button className='btn' type='primary'>SignIn</Button>
+        </Form.Item>
+      </Form>
+    </div>
     )
   }
 }
--- src/pages/User/Register.js
+++ src/pages/User/Register.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react'
-
+import Logo from 'Assets/icon.png'
+import style from './account.scss';
+import {Form, Input, Button} from 'antd';
 export default class index extends Component{
   render() {
-    return (
-      <div>
-        Register
-      </div>
-    )
+    return <div className={style.account}>
+      <img src={Logo} alt='my logo' className={style.logo} />
+      <Form className='account-form'>
+        <Form.Item label='Email'>
+          <Input/>
+        </Form.Item>
+        <Form.Item label='Password'>
+          <Input/>
+        </Form.Item>
+        <Form.Item label='Confirm Password'>
+          <Input/>
+        </Form.Item>
+        <Form.Item>
+          <Button className='btn' type='primary'>Register</Button>
+        </Form.Item>
+      </Form>
+    </div>;
   }
 }
